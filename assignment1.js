8aeeb9223824ffa2c1a9e8b33ad0ce05ef06ece0
Sounak Kundu <138959874+SKgogol@users.noreply.github.com>
2024-03-01 19:23:32 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,58 @@
+//comments can make code readable.
+
+//Welcome To 30 Days of Javascirpt
+
+/*comments can make code readable
+easy to reuse and informative */
+
+//string
+myname='Sounak';
+//boolean
+isMale=true;
+//undefined
+y=undefined;
+//Null
+x=null;
+//number
+age=20;
+
+typeof(myname);//string
+typeof(isMale);//boolean
+typeof(y);//undefined
+typeof(x);//null
+typeof(age);//number
+
+//using let
+let p;
+let k;
+let z;
+let m;
+
+const num=20;
+const year=2024;
+const cgpa=8.5;
+const sex='male';
+
+const first_name="Sounak";
+const last_name="Kundu";
+const marital_status="Unmarried";
+const country="India";
+const my_age=20;
+
+const info={
+    firstname:"Sounak",
+    lastname:"Kundu",
+    maritalstatus:"Unmarried",
+    country:"india",
+    age:20,
+};
+
+const myAge=25;
+const yourAge=35;
+
+console.log("I am ",myAge,"years old");
+console.log("Your age",yourAge,"yeras old");
+
+
+
+
